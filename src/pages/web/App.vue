5e60179edd804257fd99a7545bd68a85dc6858e6
keshanju <key.shan.ju@gmail.com>
2021-11-05 10:39:18 +0800

    动画测试， 按键修饰符

--- src/pages/web/App.vue
+++ src/pages/web/App.vue
@@ -1,3 +1,59 @@
 <template>
-  <router-view />
+  <transition :name=transitionName  mode="out-in">
+    <router-view />
+  </transition>
 </template>
+<script>
+
+export default {
+  data() {
+    return {
+      transitionName: 'slide-fade'
+    }
+  },
+
+  watch: {
+    '$route' (to, from) {
+      // const toDepth = to.path.split('/').length
+      // const fromDepth = from.path.split('/').length
+      // this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+    }
+  },
+
+  created() {
+
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+<style lang="less" scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+
+</style>
